4ef6bf59645dd85337edeaf7d0a6fdffe7506a3f
ABIMBOLA, Idunnuoluwa <72974286+thecoder-co@users.noreply.github.com>
2021-10-14 20:52:10 +0100

    fixed variable issue

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -7,20 +8,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  Map data = {};
+  Map? data;
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context)!.settings.arguments as Map;
-    print(data);
+    print(data.runtimeType);
+    print(data!['isDaytime']);
 
     //set background image
 
-    String bgImage = data['isDayTime'] ? 'daytime.jpg' : 'nightime.jpg';
-    Color bgColor = data['isDayTime'] ? Colors.blue : Colors.black54;
-
+    String bgImage = data!['isDaytime'] ? 'daytime.jpg' : 'nightime.jpg';
+    Color bgColor = data!['isDaytime'] ? Colors.blue : Colors.black54;
 
     return Scaffold(
       backgroundColor: bgColor,
@@ -29,51 +28,47 @@ class _HomeState extends State<Home> {
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/$bgImage'),
-              fit: BoxFit.cover, 
+              fit: BoxFit.cover,
             ),
           ),
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(0,120.0,0,0),
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/location');
-                  } ,
-                  icon: Icon(
-                    Icons.edit_location,
-                    color: Colors.grey[300],
-                  ),
-                  label: Text(
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
+                    ),
+                    label: Text(
                       'Edit Location',
                       style: TextStyle(
                         fontSize: 28.0,
                         letterSpacing: 2.0,
                         color: Colors.white,
                       ),
-                  )
+                    )),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(data!['location'],
+                        style: TextStyle(fontSize: 28.0, letterSpacing: 2.0)),
+                  ],
                 ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0
-                    )
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data!['time'],
+                  style: TextStyle(
+                    fontSize: 60.0,
+                    color: Colors.white,
                   ),
-                ],
-              ),
-              SizedBox(height: 20.0,),
-              Text(
-                data['time'] ,
-                style: TextStyle(
-                  fontSize: 60.0,
-                  color: Colors.white,
                 ),
-              ),
               ],
             ),
           ),
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -2,29 +2,26 @@ import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:intl/intl.dart';
 
-class WorldTime{
-
+class WorldTime {
   String? location; //location name for the ui
   String? time; //the time in that location
   String? flag; //url to an asset flag icon
   String? url; //location url for api endpoint
-  bool isDaytime ; //true or false if daytime or not
+  bool? isDaytime; //true or false if daytime or not
 
-
-
-  WorldTime({this.location, this.flag, this.url});
+  WorldTime({this.location, this.flag, this.url, this.isDaytime});
 
   Future<void> getTime() async {
-
-    try{
+    try {
       //make the request
-      Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+      Response response =
+          await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
       Map data = jsonDecode(response.body);
       //print(data);
 
       //get properties from data
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1,3);
+      String offset = data['utc_offset'].substring(1, 3);
       //print(datetime);
       //print(offset);
 
@@ -36,16 +33,9 @@ class WorldTime{
       isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
 
       time = DateFormat.jm().format(now);
-
-
-    }
-    catch(e){
+    } catch (e) {
       print('caught error: $e');
       time = 'could not get time data';
     }
-
   }
-
-
 }
-
